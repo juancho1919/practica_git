48eaa21e061669ed6da52a5260463493886a9e5b
juan sebastian sandoval bolaños <vuzulusacion@gmail.com>
2025-11-13 08:22:31 -0500

    se creo las funciones para allar las areas del cuadrado y del triangulo

---
+++ calculos.py
@@ -0,0 +1,7 @@
+#aqui se usa el lado por lado 
+def area_c(lado):
+    return lado * lado
+#aqui para hallar el area del trinagula se necesita el area y la altura dividido 2 
+def area_t(base,altura):
+    return base * altura / 2
+
