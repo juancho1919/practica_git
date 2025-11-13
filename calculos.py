20d53d5be0de71c830ad3c5105325aae2b1a8306
juan sebastian sandoval bolaños <vuzulusacion@gmail.com>
2025-11-13 08:29:25 -0500

    feat:se agregaron los comentarios en las funciones de area de triangulo

--- calculos.py
+++ calculos.py
@@ -1,7 +1,9 @@
-#aqui se usa el lado por lado 
+#aqui volvi a crear la funcion por que no se guardo del anterior compañero
 def area_c(lado):
     return lado * lado
-#aqui para hallar el area del trinagula se necesita el area y la altura dividido 2 
+#aqui para hallar el area del trinagula se necesita el area y la altura dividido 2.
+#La cree por que el anterior cmpañero no alcanzo a hacerla
+
 def area_t(base,altura):
     return base * altura / 2
 
